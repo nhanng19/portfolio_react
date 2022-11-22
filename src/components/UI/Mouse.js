d65f8c473fd41a2e6a73b028ccc010d9fdadeb55
nhan <nhanng19@gmail.com>
2022-11-21 17:08:26 -0800

    added scroll down button

---
+++ src/components/UI/Mouse.js
@@ -0,0 +1,13 @@
+import "./Mouse.css";
+
+const Mouse = () => {
+  return (
+    <span class="scroll-btn">
+      <span class="mouse">
+        <span></span>
+      </span>
+    </span>
+  );
+};
+
+export default Mouse;
