eaf3a325d9c827344be2f1a0a652b07fa7166644
nhan <nhanng19@gmail.com>
2022-11-21 17:05:47 -0800

    added Navbar component

---
+++ src/components/NavBar/RightNav.js
@@ -0,0 +1,63 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-scroll";
+
+const Ul = styled.ul`
+  list-style: none;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  li {
+    padding: 18px 10px;
+    cursor: pointer;
+  }
+  @media (max-width: 390px) {
+    flex-flow: column nowrap;
+    background: linear-gradient(112.1deg, #000000 11.4%, #2d3436 70.2%);
+    position: fixed;
+    transform: ${({ open }) => (open ? "translateX(0)" : "translateX(100%)")};
+    top: 0;
+    right: 0;
+    height: 100vh;
+    width: 300px;
+    padding-top: 3.5rem;
+    transition: transform 0.3s ease-in-out;
+    li {
+      color: #fff;
+    }
+  }
+`;
+
+const RightNav = ({ open }) => {
+  return (
+    <Ul open={open}>
+      <li>
+        <Link to="landing" spy={true} smooth={true} offset={0} duration={500}>
+          HOME
+        </Link>
+      </li>
+      <li>
+        <Link to="project" spy={true} smooth={true} offset={-30} duration={500}>
+          PROJECTS
+        </Link>
+      </li>
+      <li>
+        <Link to="about" spy={true} smooth={true} offset={50} duration={500}>
+          ABOUT
+        </Link>
+      </li>
+      <li>
+        <Link to="contact" spy={true} smooth={true} offset={50} duration={500}>
+          CONTACT
+        </Link>
+      </li>
+      <li>
+        <Link to="/" spy={true} smooth={true} offset={50} duration={500}>
+          RESUME
+        </Link>
+      </li>
+    </Ul>
+  );
+};
+
+export default RightNav;
