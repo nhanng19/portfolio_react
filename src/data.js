e77b9f7cfc480e2a326db202aaef9dd070050db8
nhan <nhanng19@gmail.com>
2022-11-21 17:08:55 -0800

    added pictures

---
+++ src/data.js
@@ -0,0 +1,43 @@
+import Simplify from "./img/simplify1.png";
+import DrawMe from "./img/homepage.png";
+import Fooder from "./img/Fooder.png";
+import Jingle from "./img/Jingle.png";
+
+export const projects = [
+  {
+    id: 1,
+    img: Simplify,
+    link: "https://simplify-apiworlds.netlify.app/summarize",
+    delay: "0",
+  },
+  {
+    id: 2,
+    img: DrawMe,
+    link: "https://draw-me.herokuapp.com/",
+    delay: "100",
+  },
+  {
+    id: 3,
+    img: Fooder,
+    link: "https://nhanng19.github.io/Fooder",
+    delay: "200",
+  },
+  {
+    id: 4,
+    img: Jingle,
+    link: "https://nhanng19.github.io/music_wordle",
+    delay: "0",
+  },
+  {
+    id: 5,
+    img: Simplify,
+    link: "https://www.google.com",
+    delay: "100",
+  },
+  {
+    id: 6,
+    img: Simplify,
+    link: "http://www.google.com",
+    delay: "200",
+  },
+];
