ed0e55e06ef20e815f8770962b8c3e70a7ff723d
nhan <nhanng19@gmail.com>
2022-11-21 17:07:53 -0800

    added socials sidebar

---
+++ src/components/Social/Social.js
@@ -0,0 +1,63 @@
+import styles from "./Social.module.css";
+import {
+  faGithub,
+  faGitlab,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Social = () => {
+  return (
+    <div className={styles.socials}>
+      <ul data-aos="fade-up" className={styles.list}>
+        <li>
+          <a
+            href="https://github.com/nhanng19"
+            aria-label="Github"
+            className={styles.link}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              className={styles.font}
+              icon={faGithub}
+              color="#DD0031"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/nhanngyn/"
+            aria-label="Github"
+            className={styles.link}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              className={styles.font}
+              icon={faLinkedin}
+              color="#DD0031"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/nhanngyn/"
+            aria-label="Github"
+            className={styles.link}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              className={styles.font}
+              icon={faGitlab}
+              color="#DD0031"
+            />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Social;
