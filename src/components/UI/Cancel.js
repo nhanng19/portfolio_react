b69a7306fe829b3e468f7846fd8d9a44cc3600ad
nhan <nhanng19@gmail.com>
2022-11-26 23:12:19 -0800

    added cancel button

---
+++ src/components/UI/Cancel.js
@@ -0,0 +1,38 @@
+const Cancel = (props) => {
+    return (
+      <div style={{cursor: "pointer"} } onClick = {props.onCancel}>
+        <svg
+        width="36"
+        height="36"
+        viewBox="0 0 36 36"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M18 33C26.25 33 33 26.25 33 18C33 9.75 26.25 3 18 3C9.75 3 3 9.75 3 18C3 26.25 9.75 33 18 33Z"
+          stroke="white"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M13.7549 22.245L22.2449 13.755"
+          stroke="white"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M22.2449 22.245L13.7549 13.755"
+          stroke="white"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+      </div>
+      
+    );
+}
+
+export default Cancel;
