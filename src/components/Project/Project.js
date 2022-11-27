4fe2ac94fc97a004d8b4c555a928dcdca46828a9
nhan <nhanng19@gmail.com>
2022-11-26 23:11:12 -0800

    added modal component

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,17 +1,36 @@
-import styles from "./Project.module.css";
 
+import styles from "./Project.module.css";
+import { useState } from "react";
+import ErrorModal from "../UI/ProjectModal";
+import { projects } from "../../data.js";
 const Project = ({ img, link, delay }) => {
+  const [modal, setModal] = useState(false);
+  const modalHandler = () => {
+    setModal(false)
+  }
   return (
-    <div data-aos-delay={delay} data-aos="fade-right" className={styles.project}>
-      <div className={styles.browser}>
-        <div className={styles.circle1}></div>
-        <div className={styles.circle2}></div>
-        <div className={styles.circle3}></div>
+    <>
+      <div
+        data-aos-delay={delay}
+        data-aos="fade-right"
+        className={styles.project}
+      >
+        <div className={styles.browser}>
+          <div className={styles.circle1}></div>
+          <div className={styles.circle2}></div>
+          <div className={styles.circle3}></div>
+        </div>
+        <img
+          onClick={() => {
+            setModal(true);
+          }}
+          src={img}
+          alt=""
+          className={styles.img}
+        />
       </div>
-      <a href={link} target="_blank" rel="noreferrer">
-        <img src={img} alt="" className={styles.img} />
-      </a>
-    </div>
+      {modal && <ErrorModal message={link} onConfirm={modalHandler} />}
+    </>
   );
 };
 
