854df7e7e4c1982e19599a9f90e11ceedd792c45
nhan <nhanng19@gmail.com>
2022-11-27 20:36:46 -0800

    added preloader before entering portfolio

---
+++ src/components/Preloader/Preloader.js
@@ -0,0 +1,182 @@
+import { useState, useEffect } from "react";
+import { gsap, CSSPlugin, Expo } from "gsap";
+import styled from "styled-components";
+
+const Preloader = () => {
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    const count = setInterval(() => {
+      setCounter((counter) =>
+        counter < 100
+          ? counter + 1
+          : (clearInterval(count), setCounter(100), reveal())
+      );
+    }, 25);
+  }, []);
+
+  const reveal = () => {
+    const t1 = gsap.timeline({});
+    t1.to(".follow", {
+      width: "100%",
+      ease: Expo.easeInOut,
+      duration: 1.2,
+      delay: 0.5,
+    })
+      .to(".hide", { opacity: 0, duration: 0.3 })
+      .to(".follow", {
+        height: "100%",
+        ease: Expo.easeInOut,
+        duration: 0.7,
+      })
+      .to(".content", { width: "100%", ease: Expo.easeInOut, duration: 0.7 });
+  };
+  return (
+    <AppContainer>
+      <Loading>
+        <Follow className="follow"></Follow>
+        <ProgressBar
+          className="hide"
+          id="progress-bar"
+          style={{ width: counter + "%" }}
+        ></ProgressBar>
+      </Loading>
+
+      <Content className="content"></Content>
+    </AppContainer>
+  );
+};
+
+const AppContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  color: #000000;
+  position: relative;
+`;
+const Loading = styled.div`
+  height: 100%;
+  width: 100%;
+  background-color: #000000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+`;
+const Follow = styled.div`
+  position: absolute;
+  background: linear-gradient(112.1deg, #000000 11.4%, #2d3436 70.2%);
+  height: 2px;
+  width: 0;
+  left: 0;
+  z-index: 2;
+`;
+
+const ProgressBar = styled.div`
+  position: absolute;
+  left: 0;
+  background-color: #fff;
+  height: 2px;
+  width: 0;
+  transition: 0.4s ease-out;
+`;
+
+const Count = styled.p`
+  font-size: 8rem;
+  font-weight: 700;
+  text-transform: uppercase;
+  position: relative;
+
+  text-shadow: 0.05em 0 0 rgba(255, 0, 0, 0.75),
+    -0.025em -0.05em 0 rgba(0, 255, 0, 0.75),
+    0.025em 0.05em 0 rgba(0, 0, 255, 0.75);
+
+  animation: glitch 500ms infinite;
+
+  span {
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+
+  span:first-child {
+    animation: glitch 650ms infinite;
+    clip-path: polygon(0 0, 100% 0, 100% 45%, 0 45%);
+    transform: translate(-0.025em, -0.0125em);
+    /* color: green; */
+    opacity: 0.8;
+  }
+
+  span:last-child {
+    animation: glitch 375ms infinite;
+    clip-path: polygon(0 80%, 100% 20%, 100% 100%, 0 100%);
+    transform: translate(0.0125em, 0.025em);
+    /* color: red; */
+    opacity: 0.8;
+  }
+
+
+  @keyframes glitch {
+    0% {
+      text-shadow: 0.05em 0 0 rgba(255, 0, 0, 0.75),
+        -0.05em -0.025em 0 rgba(0, 255, 0, 0.75),
+        -0.025em 0.05em 0 rgba(0, 0, 255, 0.75);
+    }
+    14% {
+      text-shadow: 0.05em 0 0 rgba(255, 0, 0, 0.75),
+        -0.05em -0.025em 0 rgba(0, 255, 0, 0.75),
+        -0.025em 0.05em 0 rgba(0, 0, 255, 0.75);
+    }
+    15% {
+      text-shadow: -0.05em -0.025em 0 rgba(255, 0, 0, 0.75),
+        0.025em 0.025em 0 rgba(0, 255, 0, 0.75),
+        -0.05em -0.05em 0 rgba(0, 0, 255, 0.75);
+    }
+    49% {
+      text-shadow: -0.05em -0.025em 0 rgba(255, 0, 0, 0.75),
+        0.025em 0.025em 0 rgba(0, 255, 0, 0.75),
+        -0.05em -0.05em 0 rgba(0, 0, 255, 0.75);
+    }
+    50% {
+      text-shadow: 0.025em 0.05em 0 rgba(255, 0, 0, 0.75),
+        0.05em 0 0 rgba(0, 255, 0, 0.75), 0 -0.05em 0 rgba(0, 0, 255, 0.75);
+    }
+    99% {
+      text-shadow: 0.025em 0.05em 0 rgba(255, 0, 0, 0.75),
+        0.05em 0 0 rgba(0, 255, 0, 0.75), 0 -0.05em 0 rgba(0, 0, 255, 0.75);
+    }
+    100% {
+      text-shadow: -0.025em 0 0 rgba(255, 0, 0, 0.75),
+        -0.025em -0.025em 0 rgba(0, 255, 0, 0.75),
+        -0.025em -0.05em 0 rgba(0, 0, 255, 0.75);
+    }
+  }
+
+  @media (prefers-reduced-motion: reduce) {
+    *,
+    ::before,
+    ::after {
+      animation-delay: -1ms !important;
+      animation-duration: 1ms !important;
+      animation-iteration-count: 1 !important;
+      background-attachment: initial !important;
+      scroll-behavior: auto !important;
+      transition-duration: 0s !important;
+      transition-delay: 0s !important;
+    }
+  }
+`;
+
+const Content = styled.div`
+  height: 100%;
+  width: 0;
+  position: absolute;
+  left: 0;
+  top: 0;
+  background-color: #000000;
+  padding: auto;
+  z-index: 2;
+  overflow: hidden;
+  color: #fff;
+`;
+
+export default Preloader;
