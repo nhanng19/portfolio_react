94087e6e0d067036bb937e9a4d2dffae28e5066b
nhan <nhanng19@gmail.com>
2022-11-27 20:38:55 -0800

    added conditional rendering for preloader

--- src/App.js
+++ src/App.js
@@ -4,16 +4,35 @@ import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
 import Social from "./components/Social/Social";
 import Navbar from "./components/NavBar/NavBar";
-
+import { useState, useEffect } from "react";
+import { gsap, CSSPlugin, Expo } from "gsap";
+import styled from "styled-components";
+import Preloader from "./components/Preloader/Preloader";
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 4700);
+  }, []);
+
+ 
   return (
     <>
-      <Navbar />
-      <Social />
-      <Landing />
-      <ProjectList />
-      <About />
-      <Contact />
+      {loading ? (
+        <Preloader />
+      ) : (
+        <>
+          <Navbar />
+          <Social />
+          <Landing />
+          <ProjectList />
+          <About />
+          <Contact />
+        </>
+      )}
     </>
   );
 }
