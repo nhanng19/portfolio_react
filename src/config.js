7347eea13a0abf31abf305acc1437963a2092164
nhan <nhanng19@gmail.com>
2022-11-29 14:02:16 -0800

    new files

---
+++ src/config.js
@@ -0,0 +1,9 @@
+const Config = {
+  API_KEY: "AIzaSyBbc-Ev_sRb00nwwUnfYyg2obrGjjfbnSY",
+  MAP_ID: "d477557659ce5578",
+  SERVICE_ID: "service_9cnsdic",
+  TEMPLATE_ID: "template_jlhn4l9",
+  USER_ID: "JqU-AIwKYfyySIc52",
+};
+
+export default Config;
