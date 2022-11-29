444b8e4c3eef231e79391cbcf2d28ec67499fbcb
nhan <nhanng19@gmail.com>
2022-11-28 18:19:55 -0800

    added summary and picture props

--- src/components/UI/ProjectModal.js
+++ src/components/UI/ProjectModal.js
@@ -4,21 +4,22 @@ import Card from "./Card";
 import Cancel from "./Cancel";
 import { motion } from "framer-motion";
 import parse from "html-react-parser";
+import Jingle from "../../img/Jingle.png";
 const Backdrop = (props) => {
-    const dropIn = {
-      hidden: {
-        opacity: 0,
+  const dropIn = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+      transition: {
+        duration: 0.1,
       },
-      visible: {
-        opacity: 1,
-        transition: {
-          duration: 0.1
-        }
-      },
-      exit: {
-        opacity: 0,
-      },
-    };
+    },
+    exit: {
+      opacity: 0,
+    },
+  };
   return (
     <motion.div
       variants={dropIn}
@@ -34,20 +35,35 @@ const Backdrop = (props) => {
 const ModalOverlay = (props) => {
   return (
     <Card className={classes.modal}>
+      <header className={classes.actions}>
+        <Cancel onCancel={props.onConfirm} />
+      </header>
       <div className={classes.left}>
         <div className={classes.leftWrapper}>
           <h2 className={classes.intro}>{props.description}</h2>
           <h1 className={classes.name}>{props.title}</h1>
           <div className={classes.titleItem}>{props.techno}</div>
-          <p className={classes.description}>
-           {parse(`${props.summary}`)} 
-          </p>
+          <p className={classes.description}>{parse(`${props.summary}`)}</p>
+          <div className={classes.source}>
+            <button
+              onClick={() => window.open(`${props.link}`, "_blank")}
+              type="button"
+            >
+              See Live
+            </button>
+            <button
+              onClick={() => window.open(`${props.source}`, "_blank")}
+              type="button"
+            >
+              Source Code
+            </button>
+          </div>
         </div>
       </div>
       <div className={classes.right}>
-        <footer className={classes.actions}>
-          <Cancel onCancel={props.onConfirm} />
-        </footer>
+        <div className={classes.imgContainer}>
+          <img className={classes.img} src={props.img}></img>
+        </div>
       </div>
     </Card>
   );
@@ -64,7 +80,10 @@ const ProjectModal = (props) => {
           title={props.title}
           description={props.description}
           techno={props.techno}
-          summary = {props.summary}
+          summary={props.summary}
+          link={props.link}
+          source={props.source}
+          img={props.img}
           onConfirm={props.onConfirm}
         />,
         document.getElementById("overlay-root")
